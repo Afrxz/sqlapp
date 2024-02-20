6c78b70eb99f9f793243f47ca1e98e62dba2909d
Afrxz <146107723+Afrxz@users.noreply.github.com>
2024-02-20 09:28:46 +0400

    Update sqlapp.py

--- sqlapp.py
+++ sqlapp.py
@@ -2,12 +2,9 @@ import streamlit as st
 st.title("Formatting values to be inserted into an MySql tabel")
 st.subheader("please note that this will only work if every column uses a varchar datatype")
 
-txtic= st.empty()
-txt= st.text_area(label="Enter values",height=300,max_chars=1000,placeholder="values go here")
-if txt !="":
-    txtic.empty()
 
-    st.write(txt)
+
+st.write(txt)
 
 
 
